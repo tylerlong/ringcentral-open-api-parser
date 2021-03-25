0fd79176a28b5a9b5f1003e574a577a36c295dc9
Tyler Long <tyler4long@gmail.com>
2021-03-25 16:35:11 -0700

    Refactor code

---
+++ src/types.ts
@@ -0,0 +1,42 @@
+import {OpenAPIV3} from 'openapi-types';
+
+export type ParseResult = {
+  models: Model[];
+  paths: Path[];
+};
+
+export type Model = {
+  name: string;
+  description?: string;
+  fields: Field[];
+};
+
+export type Path = {
+  endpoint: string;
+  paths: string[];
+  parameter?: string;
+  operations: Operation[];
+};
+
+export type Operation = {
+  method: string;
+};
+
+export type Field = {
+  name: string;
+  type?: string;
+  $ref?: string;
+  description?: string;
+  enum?: string[];
+  example?: string;
+  format?: string;
+  items?: Field;
+  default?: string | boolean;
+  minimum?: number;
+  maximum?: number;
+  required?: boolean;
+};
+
+export type SchemaDict = {
+  [key: string]: OpenAPIV3.SchemaObject;
+};
