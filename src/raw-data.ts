768ad45bec613e668483e8a9f2ce8a73990ee16a
Tyler Long <tyler4long@gmail.com>
2023-03-28 12:59:46 -0700

    Resolve requestBody $ref

--- src/raw-data.ts
+++ src/raw-data.ts
@@ -8,8 +8,15 @@ export const getRawData = (filePath: string) => {
   const doc = load(readFileSync(filePath, 'utf8')) as OpenAPIV3.Document;
   const operations: RawOperation[] = [];
   Object.values(doc.paths).forEach((pathObject) => {
-    Object.values(pathObject!).forEach((ops) => {
-      operations.push(ops as RawOperation);
+    Object.values(pathObject!).forEach((_ops) => {
+      const ops = _ops as RawOperation;
+      // Resolve $ref in requset bodies
+      if (ops.requestBody && '$ref' in ops.requestBody) {
+        ops.requestBody = doc.components!.requestBodies![
+          (ops.requestBody.$ref as string).split('/').pop()!
+        ] as OpenAPIV3.RequestBodyObject;
+      }
+      operations.push(ops);
     });
   });
   return { doc, operations };
