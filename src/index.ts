2a92ace074c4d0fc0b272fc70ff1e9146ced3064
Tyler Long <tyler4long@gmail.com>
2023-08-22 13:34:18 -0700

    Release 0.11.6

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { prepareModels } from './models';
 import { preparePaths } from './paths';
 import { getRawData } from './raw-data';
 
+/**
+ * Prepares the specification by extracting the necessary data from the OpenAPI document.
+ *
+ * @param filePath - The path to the OpenAPI document file.
+ * @returns An object containing the prepared models and paths extracted from the OpenAPI document.
+ */
 export const prepareSpec = (filePath: string) => {
   const { doc, operations } = getRawData(filePath);
   const models = prepareModels(doc, operations);
