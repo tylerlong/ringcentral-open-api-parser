2facc79206562251b9f6d7d1d513af0e0a3ec3bc
Tyler Liu <tyler4long@gmail.com>
2025-03-11 13:19:29 -0700

    Handle anonymous response types

--- src/models/collect/responses.ts
+++ src/models/collect/responses.ts
@@ -1,30 +1,45 @@
-import type { OpenAPIV3 } from 'openapi-types';
+import type { OpenAPIV3 } from "openapi-types";
 
-import type { NamedSchema } from '../../types';
-import { capitalizeFirstLetter } from '../../utils';
+import type { NamedSchema } from "../../types";
+import { capitalizeFirstLetter } from "../../utils";
 
 export const collectResponses = (doc: OpenAPIV3.Document) => {
   const schemas: NamedSchema[] = [];
   Object.values(doc.paths).forEach((pathContent) => {
-    for (const method of ['get', 'post', 'put', 'delete', 'patch']) {
+    for (const method of ["get", "post", "put", "delete", "patch"]) {
       if (!(method in pathContent!)) {
         continue;
       }
-      for (const response of Object.values(pathContent![method].responses) as OpenAPIV3.OperationObject[]) {
-        if (!('content' in response)) {
+      for (
+        const response of Object.values(
+          pathContent![method].responses,
+        ) as OpenAPIV3.OperationObject[]
+      ) {
+        if (!("content" in response)) {
           continue;
         }
         const responseContent = response.content!;
-        if (responseContent['application/json']?.schema) {
-          const schema = responseContent['application/json'].schema!;
+        if (responseContent["application/json"]?.schema) {
+          const schema = responseContent["application/json"].schema!;
           const multi = schema.allOf ?? schema.anyOf ?? schema.oneOf;
           if (multi) {
-            const name = `${capitalizeFirstLetter(pathContent![method].operationId!)}Response`;
+            const name = `${
+              capitalizeFirstLetter(pathContent![method].operationId!)
+            }Response`;
             schema.$ref = `#/components/schemas/${name}`;
             schemas.push({ ...schema, name });
             delete schema.allOf;
             delete schema.anyOf;
             delete schema.oneOf;
+          } else if (schema.type === "object") { // anonymous
+            const name = `${
+              capitalizeFirstLetter(pathContent![method].operationId!)
+            }Response`;
+            schema.$ref = `#/components/schemas/${name}`;
+            schemas.push({ ...schema, name });
+            delete schema.type;
+            delete schema.properties;
+            delete schema.required;
           }
         }
       }
